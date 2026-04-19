00c1c84344779dfa29ca8f16422446bc6ea1d6a0
Gustavo Uchôa <gustavo.passos@terceirizados.celcoin.com.br>
2026-04-19 08:38:10 -0300

    Alterado url frontend

--- backend/custosMensais.Api/Program.cs
+++ backend/custosMensais.Api/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
             .WithOrigins(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "https://frontend.gustavouchoa.cloud",
+                "https://custos.gustavouchoa.cloud",
                 "https://backend.gustavouchoa.cloud")
             .AllowAnyHeader()
             .AllowAnyMethod();
